850470075d9c809cc6353a867a3c47bbbff25a56
lobanovamc <lobanova.mc@gmail.com>
2026-04-16 14:35:29 -0700

    Enhance greeting function with color and age features
    Updated greeting function to include favorite color and age increment.

---
+++ functions-exercise.py
@@ -0,0 +1,22 @@
+# 1. Add new print statement - on a new line
+#    which says 'We hear you like the color xxx! xxx is a string with color 
+# 2. extend the function with another  input parameter 'color', that defaults to 'red' 
+# 3. Capture the color via an input box as variable:color 
+# 4. Change the 'You are xx!' text to say 'you will be xx+1 years old next birthday 
+#  adding 1 to the age
+# 5. Capitalize first letter of the 'name', and rest are small caps 
+# 6. Favorite color should be in lowercase 
+
+
+def greeting(name, age, color='red'):
+    print(f'Hello {name}, you will be {age+1} years old next birthday!')
+    print(f'We hear you like the color {color}!')
+
+name = input('Enter your name: ')
+name = name.capitalize()
+age = input('Enter your age: ')
+age=int(age)
+color = input('Enter your favourite color: ')
+color = color.lower()
+
+greeting(name, age, color)
